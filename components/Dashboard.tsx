504da2d6ff472fd545a3e5d8b123004d6928827e
M-Hamza-WebDev <m.hamza.developer@gmail.com>
2023-06-06 03:38:44 +0500

    Filter by any value added

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import DashboardOverview from "./overview";
 import { Type } from "typescript";
+import { log } from "console";
 type DataType = {
   // Define the properties and their types for your data structure
   // Example:
@@ -39,19 +40,36 @@ const Dashboard = () => {
     if (value) {
       if (name === "CategoryFilter") {
         const categoryData = tableData?.filter(
-          (item:any) => item.category.includes(value) 
+          (item:any) => item.category.toLowerCase().includes(value.toLowerCase()) 
         );
         setFilteredData(categoryData);
       } else if (name == "BrandFilter") {
-        const brandData = tableData?.filter((item:any) => item.brand.includes(value));
+        const brandData = tableData?.filter((item:any) => item.brand.toLowerCase().includes(value.toLowerCase));
         setFilteredData(brandData);
       }
       else {
-        // const anyData = tableData.filter((item) => item == value);
-        // setFilteredData(anyData);
-
+      
+        const anyData = tableData.filter(item => {
+          // Get an array of values from the object properties
+          const values = Object.values(item);
+          console.log(values,"values");
+          
+          
+          // Check if any value matches the specified value (case-insensitive)
+          return values.some(val => {
+            if (typeof val === 'string') {
+              return val.toLowerCase().includes(value.toLowerCase());
+            } else if (typeof val === 'number') {
+              return val === Number(value);
+            }
+            return false;
+          });
+        });
+        
+        
+        setFilteredData(anyData);
      
-          // setFilteredData(filteredProducts);
+         
       }
     } else {
       setFilteredData(tableData);
@@ -92,8 +110,10 @@ const Dashboard = () => {
             }}
           />
         </div>
-        {/* <div>
+        <div>
+        <h1 className="pb-2 font-bold">Filter by any value:</h1>
           <input
+           className="p-2 rounded-md focus:outline-none"
             type="text"
             placeholder="Search"
             name="SearchQuery"
@@ -104,7 +124,7 @@ const Dashboard = () => {
         
         }
           />
-        </div> */}
+        </div>
       </div>
       <div className="overflow-x-auto rounded-md">
         <table className="min-w-full divide-y divide-gray-200 rounded-md">
